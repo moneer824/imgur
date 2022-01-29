59cc446040872cc85ca328269dd20051f4b0df85
moneer824 <87421838+moneer824@users.noreply.github.com>
2022-01-29 15:50:34 +0530

    full screen

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var parent = document.getElementById("parent");
+var hide = document.getElementById("hide");
+var fullSc = document.getElementById("full-screen");
 var page = 1;
 
 async function unsplash(query = "random") {
@@ -15,7 +17,17 @@ function showImages(data) {
     // console.log(item.links.download);
     let div = document.createElement("div");
     let img = document.createElement("img");
-    img.src = item.urls.regular;
+    img.src = item.urls.small;
+    div.onclick = () => {
+      fullSc.innerHTML = null;
+
+      let img = document.createElement("img");
+      img.src = item.urls.regular;
+      fullSc.style.display = "block";
+      hide.style.display = "block";
+      fullSc.append(img);
+      console.log(item.urls.regular);
+    };
     div.append(img);
     parent.append(div);
   });
@@ -30,12 +42,16 @@ function search() {
 
 function more() {
   let inp = document.getElementById("search-inp").value;
-  page++
+  page++;
   console.log(inp);
-if (inp.length === 0) {
-    inp = "random"
-}
+  if (inp.length === 0) {
+    inp = "random";
+  }
   unsplash(inp);
 }
 
+function hideImg() {
+  fullSc.style.display = "none";
+  hide.style.display = "none";
+}
 unsplash();
