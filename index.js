fbcd9c4adbfd82d456e1effc83071e3f683210d2
moneer824 <87421838+moneer824@users.noreply.github.com>
2022-01-30 18:05:26 +0530

    done

--- index.js
+++ index.js
@@ -8,25 +8,21 @@ async function unsplash(query = "random") {
     `https://api.unsplash.com/search/photos/?page=${page}&query=${query}&per_page=16&client_id=oSepL4SgiY-kAVU0aUW_w3IdwJL9PlnDC-SWpXd7PSY`
   );
   let data = await res.json();
-  console.log(data.results);
   showImages(data.results);
 }
 
 function showImages(data) {
   data.forEach((item) => {
-    // console.log(item.links.download);
     let div = document.createElement("div");
     let img = document.createElement("img");
-    img.src = item.urls.small;
+    img.src = item.urls.regular;
     div.onclick = () => {
       fullSc.innerHTML = null;
-
       let img = document.createElement("img");
       img.src = item.urls.regular;
       fullSc.style.display = "block";
       hide.style.display = "block";
       fullSc.append(img);
-      console.log(item.urls.regular);
     };
     div.append(img);
     parent.append(div);
@@ -36,14 +32,12 @@ function showImages(data) {
 function search() {
   parent.innerHTML = null;
   let inp = document.getElementById("search-inp").value;
-  console.log(inp);
   unsplash(inp);
 }
 
 function more() {
   let inp = document.getElementById("search-inp").value;
   page++;
-  console.log(inp);
   if (inp.length === 0) {
     inp = "random";
   }
